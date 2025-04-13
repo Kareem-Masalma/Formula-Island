b6ccc38283e98582ba4bd04998fdeedceead11bf
Kareem-Masalma <1220535@student.birzeit.edu>
2025-04-14 01:24:41 +0300

    Finishing Background scene and Create Dashboard activity

--- app/src/main/java/com/example/formulaisland/Background.java
+++ app/src/main/java/com/example/formulaisland/Background.java
@@ -1,6 +1,9 @@
 package com.example.formulaisland;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Background extends AppCompatActivity {
+
+    private Button btnShop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,23 @@ public class Background extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViews();
+        goShopping();
     }
+
+    private void findViews() {
+        btnShop = findViewById(R.id.btnShop);
+    }
+
+    private void goShopping() {
+        btnShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Background.this, Dashboard.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
